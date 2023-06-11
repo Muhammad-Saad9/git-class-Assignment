8e3af192dbaa371ebb67743c78de1bf3e3ce1624
saad <saadthedesginer314@gmail.com>
2023-06-11 20:36:42 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main(){
+  String gmail="saadthedesigner314@gmail.com";
+  var password="saad1234";
+  if(gmail=="saadthedesigner314@gmail.com"&& password=="saad1234");
+  print("Succesfully logged in");
+  if(gmail=="saadthedesigner314@gmail.com "&& password!="saad1234"){
+print("Something went wrong!");
+  }
+}
+ 
