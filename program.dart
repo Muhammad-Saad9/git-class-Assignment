5dc5ebf55be0b3f096ff71933ed2f52b159a6fd9
saad <saadthedesginer314@gmail.com>
2023-06-12 17:41:54 +0500

    practicecomplete

---
+++ program.dart
@@ -0,0 +1,9 @@
+void main(){
+  //write a program to check wheter the number is odd or even//
+  num n1=24;
+  if(n1%2==0){
+    print("It is an Odd number");
+  }else{
+    print("It is an Even number");
+  }
+}
