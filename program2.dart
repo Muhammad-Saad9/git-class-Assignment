50bf1bb4bc4caecb67f2dc85d2a32fe543dc94ca
MUhammad Saad <saadthedesigner314@gmail.com>
2023-06-13 18:44:40 +0500

    second commit

---
+++ program2.dart
@@ -0,0 +1,24 @@
+void main(){
+  //write a program that identify the character is Alphabetic or  Numeric//
+  //Give 2 character and This program will Identify Whether the character is Alphabetic or Numeric//
+  var character=12;
+  var character2="s";
+  if(character=='a'||character=='b'||character=='c'||character=='d'||character=='e'||character=='f'||character=='g'||character=='h'||character=='i'||character=='j'||character=='k'||character=='l'||
+  character=='m'||character=='n'||character=='o'||character=='p'||character=='q'||character=='r'||character=='s'||character=='t'||character=='u'||character=='v'||character=='w'||character=='x'||
+  character=='y'||character=='z'||character=='A'||character=='B'||character=='C'||character=='D'||character=='E'||character=='F'||character=='G'||character=='H'||character=='I'||character=='J'
+  ||character=='K'||character=='L'||character=='M'||character=='N'||character=='O'||character=='P'||character=='Q'||character=='R'||character=='S'||character=='T'||character=='U'||character=='W'||character=='w'||character=='x'||
+  character=='Y'||character=='Z'){
+    print("The character you have entered is an Alphabet");
+  }else{
+    print("The character you have entered is a Number");
+  }
+    if(character2=='a'||character2=='b'||character2=='c'||character2=='d'||character2=='e'||character2=='f'||character2=='g'||character2=='h'||character2=='i'||character2=='j'||character2=='k'||character2=='l'||
+  character2=='m'||character2=='n'||character2=='o'||character2=='p'||character2=='q'||character2=='r'||character2=='s'||character2=='t'||character2=='u'||character2=='v'||character2=='w'||character2=='x'||
+  character2=='y'||character2=='z'||character2=='A'||character2=='B'||character2=='C'||character2=='D'||character2=='E'||character2=='F'||character2=='G'||character2=='H'||character2=='I'||character2=='J'
+  ||character2=='K'||character2=='L'||character2=='M'||character2=='N'||character2=='O'||character2=='P'||character2=='Q'||character2=='R'||character2=='S'||character2=='T'||character2=='U'||character2=='W'||character2=='w'||character2=='x'||
+  character2=='Y'||character2=='Z'){
+    print("The character you have entered is an Alphabet");
+  }else{
+    print("The character you have entered is a Number");
+  }
+}
