e57b7e416b2f7e8756a63f750b4b58570543ef93
MUhammad Saad <saadthedesigner314@gmail.com>
2023-06-14 21:51:29 +0500

    ThirdComplete

---
+++ program3.dart
@@ -0,0 +1,9 @@
+void main(){
+  //write a program that identfy the player if player=98 or 97 then print Match started if playersjoined are below 97 then print finding a match//
+  var playerjoined=95;
+  if(playerjoined>=98){
+    print("Match Started");
+  }if(playerjoined<=97){
+    print("Finding a Match");
+  }
+  }
