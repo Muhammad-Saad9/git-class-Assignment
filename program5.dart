6007ae6834b82cc2a951ca3b898fb0f44bf3ca12
MUhammad Saad <saadthedesigner314@gmail.com>
2023-06-16 22:25:50 +0500

    Home Task5

---
+++ program5.dart
@@ -0,0 +1,14 @@
+void main(){
+  //program that tell you the price of a product of this store...example.//
+  var name="Female Clothes in Best Prices";
+  num prdct1=1500;
+  num prdct2 =1200;
+  var cstmr="$prdct1";
+  print(name);
+  if(cstmr==prdct1){
+ print("The price of product1 is $prdct1");
+  }
+  else{
+    print("The price of product1 is $prdct2");
+  }
+}
