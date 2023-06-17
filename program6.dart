b795d9313f241616da954efedbf5df1df773e201
MUhammad Saad <saadthedesigner314@gmail.com>
2023-06-17 23:02:53 +0500

    Calculator

---
+++ program6.dart
@@ -0,0 +1,23 @@
+void main(){
+  //write a program that work like a calculator//
+    int num1=34;
+  int num2=54;
+  var oprt1=num1+num2;
+  var oprt2=num1-num2;
+  var oprt3=num1*num2;
+  var oprt4=num1/num2;
+  var user=oprt2;
+  print("select Operation");
+  if(user==oprt1){
+    print(oprt1);
+  }
+   if(user==oprt2){
+    print(oprt2);
+  }
+   if(user==oprt3){
+    print(oprt3);
+  }
+   if(user==oprt4){
+    print(oprt4);
+  }
+}
